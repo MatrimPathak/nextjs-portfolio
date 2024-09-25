be53506317ee1cb2131ec91ff72ee4bda3544363
Matrim Pathak <matrimpathak1999@gmail.com>
2024-09-25 22:46:12 +0530

    fixed more escaped characters

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -139,7 +139,7 @@ export const HeroSection = () => {
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming designs into functional,
-            high-performing web applications. Let's discuss your next project.
+            high-performing web applications. Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col items-center mt-8 gap-4 md:flex-row justify-center">
@@ -149,7 +149,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white text-gray-900 bg-white px-6 h-12 rounded-xl">
             <span>👋</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -13,7 +13,7 @@ const testimonials = [
   {
     name: "Alex Turner",
     position: "Marketing Manager @ TechStartups",
-    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
+    text: "Alex was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We&apos;re thrilled with the results!",
     avatar: memojiAvatar1,
   },
   {
@@ -25,19 +25,19 @@ const testimonials = [
   {
     name: "Daniel White",
     position: "CEO @ InnovateCo",
-    text: "Alex's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
+    text: "Alex&apos;s ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn&apos;t be happier.",
     avatar: memojiAvatar3,
   },
   {
     name: "Emily Carter",
     position: "Product Manager @ GlobalTech",
-    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
+    text: "Alex is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We&apos;re already seeing positive feedback from our customers.",
     avatar: memojiAvatar4,
   },
   {
     name: "Michael Brown",
     position: "Director of IT @ MegaCorp",
-    text: "Alex's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
+    text: "Alex&apos;s work on our website has been nothing short of exceptional. He&apos;s a talented developer who is also a great communicator. We highly recommend him.",
     avatar: memojiAvatar5,
   },
 ];
@@ -49,7 +49,7 @@ export const TestimonialsSection = () => {
         <SectionHeader
           title="What Clients Say About Me"
           eyebrow="Happy Clients"
-          description="Don't just take my word for it. See what my clients have to say about my work."
+          description="Don&apos;t just take my word for it. See what my clients have to say about my work."
         />
         <div className="mt-12 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)] lg:mt-20 py-4">
           <div className="flex flex-none gap-8 pr-8 animate-move-left [animation-duration:90s] hover:[animation-play-state:paused]">
